695e8a0979c7e19d717c160040a9f949a14f24ef
groupCMPR <144290496+groupCMPR@users.noreply.github.com>
2023-11-20 12:59:49 -0800

    Update Union_Intersect_Heap.h

--- Union_Intersect_Heap.h
+++ Union_Intersect_Heap.h
@@ -27,7 +27,7 @@ public:
 	void set_Intersect(const bool is_max);
 
 	//Accessors, for data hiding into friend function
-	bool get_Empty();
+	const bool get_Empty();
 	string get_Prompt_Action() const;
 	string get_Prompt() const;
 	typename vector<T>::const_iterator get_First_Begin() const;
