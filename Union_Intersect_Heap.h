34d301a43f6ea7f86550059396a1c1709cdf1989
groupCMPR <144290496+groupCMPR@users.noreply.github.com>
2023-12-04 15:52:36 -0800

    Update Union_Intersect_Heap.h

--- Union_Intersect_Heap.h
+++ Union_Intersect_Heap.h
@@ -11,16 +11,16 @@
 using namespace std;
 
 template <typename T>
-class Union_Intersect_Heap {
+class Union_or_Intersect_Heap {
 private:
 	vector<T> first_heap;
 	vector<T> second_heap;
 	vector<T> final_heap;
-	string prompt_action;
-	string prompt;
+	string Union_or_Intersect;
+	string Max_or_Min;
 public:
 	//constructor
-	Union_Intersect_Heap(const string& prompt);
+	Union_or_Intersect_Heap(const string& prompt);
 
 	//Mutators
 	void set_Heap();
@@ -29,8 +29,8 @@ public:
 
 	//Accessors, for data hiding into friend function
 	bool is_Empty() const;
-	string get_Prompt_Action() const;
-	string get_Prompt() const;
+	string get_string_Union_or_Intersect() const;
+	string get_string_Max_or_Min() const;
 	typename vector<T>::const_iterator get_First_Begin() const;
 	typename vector<T>::const_iterator get_First_End() const;
 	typename vector<T>::const_iterator get_Second_Begin() const;
@@ -40,7 +40,7 @@ public:
 
 	//friend
 	template <typename T>
-	friend ostream& operator<< (ostream& out, const Union_Intersect_Heap<T>& obj);
+	friend ostream& operator<< (ostream& out, const Union_or_Intersect_Heap<T>& obj);
 };
 
 #include "Union_Intersect_Heap.template"
